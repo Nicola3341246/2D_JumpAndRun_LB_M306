c293b51c221cf2990b4b4ca8f314a88f0f6349ab
TOP997 <89131744+TOP997@users.noreply.github.com>
2023-01-24 10:59:37 +0100

    idk

--- Assets/Scripts/Player/EnemyMove.cs
+++ Assets/Scripts/Player/EnemyMove.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    [SerializeField] Rigidbody2D EnemyBody;
+    [SerializeField] float walkSpeed;
+    float walkDirection;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,7 @@ public class EnemyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyBody.velocity= Vector3.zero;
+
     }
 }
