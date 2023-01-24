88216c1be00457c341b314f9603a311a2a8f7b6e
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2023-01-24 10:58:50 +0100

    Player Jump

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -8,12 +9,22 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] float walkSpeed;
     float walkDirection;
 
+    [SerializeField] float jumpHeight;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] BoxCollider2D playerBox;
+
 
 
     void Update()
     {
         MovePlayer();
         JumpPlayer();
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D isOnGround = Physics2D.BoxCast(playerBox.bounds.center, playerBox.bounds.size + new Vector3(0.1f, 0.1f, 0.1f), 0, Vector2.down, 0.01f, groundLayer);
+        return isOnGround.collider != null;
     }
 
     private void MovePlayer()
@@ -24,6 +35,10 @@ public class PlayerMove : MonoBehaviour
 
     private void JumpPlayer()
     {
-
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerBody.velocity = new Vector2(playerBody.velocity.x, jumpHeight);
+            Debug.Log("dfzaugskdfgefkuzgglzksdkgzufzuergzagzfrgaakreeae");
+        }
     }
 }
