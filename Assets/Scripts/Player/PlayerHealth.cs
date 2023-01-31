562cc27d10670351e4e252edcc536a12d8ef0d4b
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2023-01-31 08:51:29 +0100

    Zielflagge und win

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] string zielTag;
+    [SerializeField] GameObject winText;
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == zielTag)
+        {
+            winText.SetActive(true);
+        }
+    }
+}
